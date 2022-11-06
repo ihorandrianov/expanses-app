cb5c3651728cf73dda4ad97ee92d9371b643651a
Ihor Andrianov <andrianoia@gmail.com>
2022-11-06 17:53:48 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { PrismaExceptionFilter } from './exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new PrismaExceptionFilter());
+
+  await app.listen(3000);
+}
+bootstrap();
